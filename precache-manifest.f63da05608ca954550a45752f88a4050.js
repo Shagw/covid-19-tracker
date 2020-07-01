3f184ab9529136eef5945b53f05d0b630a5b75c7
Shashank Agarwal <shashanka141@gmail.com>
2020-07-01 10:35:15 +0530

    Updates

---
+++ precache-manifest.f63da05608ca954550a45752f88a4050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec37c2769854cc8be4caa2aba73e11b",
+    "url": "/covid19-20/index.html"
+  },
+  {
+    "revision": "a9ad9df58c221503204c",
+    "url": "/covid19-20/static/css/main.35705e6f.chunk.css"
+  },
+  {
+    "revision": "7f456f99b870d60163bb",
+    "url": "/covid19-20/static/js/2.573c3529.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-20/static/js/2.573c3529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ad9df58c221503204c",
+    "url": "/covid19-20/static/js/main.f17a6f18.chunk.js"
+  },
+  {
+    "revision": "33687d1e31790802cbe2",
+    "url": "/covid19-20/static/js/runtime-main.f0807cdd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-20/static/media/image.d7265326.png"
+  }
+]);
